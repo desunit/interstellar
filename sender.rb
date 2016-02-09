20d58b3ac4d195b315d7f049d638321b2963ef94
CarsMobileApp <carsmobileapp@mobileci.cars.com>
2016-02-09 11:29:29 -0600

    Updated to match new csv format

--- sender.rb
+++ sender.rb
@@ -4,9 +4,13 @@ require 'date'
 require 'csv'
 require 'yaml'
 
+$stdout.reopen("out.txt", "w")
+$stderr.reopen("err.txt", "w")
+
+Dir.chdir('/Users/carsmobileapp/interstellar')
 CONFIG = YAML.load_file('./secrets/secrets.yml')
 
-date = Date.today-2
+date = Date.today-1
 
 file_date = date.strftime("%Y%m")
 csv_file_name = "reviews_#{CONFIG["package_name"]}_#{file_date}.csv"
@@ -17,8 +21,9 @@ class Slack
   def self.notify(message)
     payload1 = {}
   payload1["text"] = 'You have ' + message.length.to_s + ' new Android reviews'
-  payload1["unfurl_links"] = true
-  payload1["unfurl_media"] = true
+  payload1["username"] = "AndroidBot"
+  payload1["icon_url"] = "http://www.android.com/media/android_vector.jpg"
+
   payload1["attachments"] = message
   RestClient.post CONFIG["slack_url"], {
       payload: payload1.to_json
@@ -57,7 +62,7 @@ class Review
     end
   end
 
-  attr_accessor :text, :title, :submitted_at, :original_subitted_at, :rate, :device, :url, :version, :edited
+  attr_accessor :text, :title, :submitted_at, :original_subitted_at, :rate, :device, :url, :version, :edited, :version_name
 
   def initialize data = {}
     @text = data[:text] ? data[:text].to_s.encode("utf-8") : nil
@@ -70,12 +75,13 @@ class Review
     @device = data[:device] ? data[:device].to_s.encode("utf-8") : nil
     @url = data[:url].to_s.encode("utf-8")
     @version = data[:version].to_s.encode("utf-8")
+    @version_name = data[:version_name].to_s.encode("utf-8")
     @edited = data[:edited]
   end
 
   def notify_to_slack
     if text || title
-      message = "*Rating: #{rate}* | version: #{version} | posted: #{submitted_at}\n #{[title, text].join(" ")}\n <#{url}|Google play>"
+      message = "*Rating: #{rate}* | version: #{version_name} | posted: #{submitted_at}\n #{[title, text].join(" ")}\n <#{url}|Google play>"
       Slack.notify(message)
     end
   end
@@ -91,7 +97,7 @@ class Review
 
     [
       "\n\n#{stars}",
-      "Version: #{version} | #{date}",
+      "Version: #{version_name} | #{date}",
       "#{[title, text].join(" ")}",
       "<#{url}|Google play>"
     ].join("\n")
@@ -100,7 +106,7 @@ class Review
   def build_attachment
   color = if self.rate > 3
         "good"
-      elsif self.rate = 3
+      elsif self.rate == 3
         "warning"
       else
         "danger"
@@ -112,12 +118,10 @@ class Review
            end
 
     stars = rate.times.map{"★"}.join + (5 - rate).times.map{"☆"}.join
-
-    text = [
+    attText = [
       "\n\n#{stars}",
-      "Version: #{version} | #{date}",
-      "#{[title, text].join(" ")}",
-      "<#{url}|Google play>"
+      "Version: #{version_name} | #{date}",
+      "#{text}"
     ].join("\n")
   attachment = {}
   attachment['fallback'] = if self.title.nil?
@@ -127,26 +131,30 @@ class Review
               end
   attachment['title'] = self.title
   attachment['color'] = color
-  attachment['text'] = text
+  attachment['text'] = attText
+  attachment["unfurl_links"] = true
+  attachment["unfurl_media"] = true
   return attachment
   end
 end
 
 CSV.foreach(csv_file_name, encoding: 'bom|utf-16le', headers: true) do |row|
   # If there is no reply - push this review
-  if row[11].nil?
+  if row[12].nil?
     Review.collection << Review.new({
-      text: row[10],
-      title: row[9],
-      submitted_at: row[6],
-      edited: (row[4] != row[6]),
-      original_subitted_at: row[4],
-      rate: row[8],
-      device: row[3],
-      url: row[14],
+      text: row[11],
+      title: row[10],
+      submitted_at: row[7],
+      edited: (row[5] != row[7]),
+      original_subitted_at: row[5],
+      rate: row[9],
+      device: row[4],
+      url: row[15],
       version: row[1],
+      version_name: row[2]
     })
   end
 end
 
 Review.send_reviews_from_date(date)
+print "Script ran #{Date.today}\n"
